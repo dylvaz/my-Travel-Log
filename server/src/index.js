25907b9020b407798314b0f4d45e79521391f1dd
dylvaz <vazquezadylan@gmail.com>
2020-12-17 00:33:05 -0800

    implemented express app and middlewares

---
+++ server/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`connected to ${process.env.MONGO_URI} 🥳`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const port = process.env.PORT || 4111;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from home 🎉',
+  });
+});
+
+app.use('/api/logs', logs);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errHandler);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port} 🎉`);
+});
