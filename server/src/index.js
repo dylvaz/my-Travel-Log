4d33c0bdfd102d3582fe1e89faa52b5a1cf3e94c
dylvaz <vazquezadylan@gmail.com>
2020-12-17 12:43:50 -0800

    added options to mongoose connection

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,12 @@ const logs = require('./api/logs');
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => {
     console.log(`connected to ${process.env.MONGO_URI} 🥳`);
   })
